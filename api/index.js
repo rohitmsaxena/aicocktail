a181c697f9b620df65ff4c6b15d17f49fb15a74c
Rohit Saxena <rohitmsaxena@gmail.com>
2023-03-14 00:06:34 -0700

    - added db.json

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+app.use(express.json());
 app.use("/ingredients", require("./ingredients"));
 app.use("/auth", require("./authentication"));
 
--- api/ingredients.js
+++ api/ingredients.js
@@ -1,14 +1,38 @@
 var express = require("express");
 var router = express.Router();
+var fs = require("fs");
 
 //Middle ware that is specific to this router
-router.use(function timeLog(req, res, next) {
-  console.log("Time: ", Date.now());
-  next();
+// router.use(function timeLog(req, res, next) {
+//   console.log("Time: ", Date.now());
+//   console.log(req.body);
+//   next();
+// });
+
+router.get("/list", (req, res) => {
+  res.send(db());
 });
-router.get("/", (req, res) => {
-  console.log("in ingredients");
-  res.send("Hi from ingredients!");
+
+router.put("/list", (req, res) => {
+  const ingredients = db();
+  if (
+    !ingredients.map((ingredient) => ingredient.name).includes(req.body.name)
+  ) {
+    const update = JSON.stringify([req.body, ...ingredients]);
+    fs.writeFile("./resources/db.json", update, "utf-8", (error) => {
+      if (error) res.sendStatus(401);
+    });
+  }
+  res.sendStatus(200);
 });
+
+router.delete("/list", (req, res) => {
+  res.send(200);
+});
+
+const db = () => {
+  delete require.cache[require.resolve("./resources/db.json")];
+  return require("./resources/db.json");
+};
 
 module.exports = router;
